55f9508c7f4981397adb0215e927111c268e2513
ALYAMBR <rdvnkt@yandex.ru>
2020-01-31 17:41:26 +0700

    main modified

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 import cv2
-import network_functions as nf
+import filework_functions as ff
+import time
+from time import sleep
 
-# Pretrained classes in the model
-"""classNames = {0: 'background',
-              1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
-              7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
-              13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
-              18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
-              24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
-              32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
-              37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
-              41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
-              46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
-              51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
-              56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
-              61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
-              67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
-              75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
-              80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
-              86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'}
-"""
+#TODO uncomment it of raspberry
+"""from picamera import PiCamera
 
-classNames = {0: 'background', 1: 'person'}
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+camera.capture('image.jpg')
+camera.stop_preview()
+
+classNames = {0: 'background', 1: 'person'}"""
 
 
 def id_class_name(class_id, classes):
@@ -30,11 +21,13 @@ def id_class_name(class_id, classes):
             return value
 
 
+time_start = time.time()
+
 # Loading model
 model = cv2.dnn.readNetFromTensorflow('models/frozen_inference_graph_mobile.pb',
                                       'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
 
-image = cv2.imread("images/" + nf.find_last_shot_addr())
+image = cv2.imread(ff.find_last_shot_addr())
 
 image_height, image_width, _ = image.shape
 
@@ -56,13 +49,7 @@ for detection in output[0, 0, :, :]:
             box_height = int(detection[6] * image_height)
             human_x = int((box_x + box_width) / 2)
             human_y = int((box_y + box_height) / 2)
-
-            """cv2.rectangle(image,
-                          (int(box_x), int(box_y)),
-                          (int(box_width), int(box_height)),
-                          (50, 230, 50),
-                          thickness=1)"""
-
+            """
             cv2.circle(image,
                        (human_x, human_y),
                        8,
@@ -74,13 +61,15 @@ for detection in output[0, 0, :, :]:
                         cv2.FONT_HERSHEY_COMPLEX,
                         (.001 * image_width),
                         (0, 255, 255),
-                        thickness=2)
-            print("Человек номер ", people_count, " находится в (", human_x, ", ", human_y, ")")
+                        thickness=2)"""
+            print("Human number ", people_count, " is in (", human_x, ", ", human_y, ")")
+time_end = time.time()
+print("Time of work in seconds: ", time_end - time_start)
+print("People count: ", people_count)
 
-print("На картинке людей примерно: ", people_count)
-
-cv2.imshow('Processed image', image)
+"""cv2.imshow('Processed image', image)
 cv2.imwrite("image_box_text.jpg", image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+"""
